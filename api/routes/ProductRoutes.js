243733ab77462e908785c134b5c40d997886e9c6
adalbertopc <adalbertinho90@gmail.com>
2021-05-15 01:30:00 -0700

    feat: add put and delete routes

--- api/routes/ProductRoutes.js
+++ api/routes/ProductRoutes.js
@@ -5,5 +5,7 @@ const verifyToken = require('../middlewares/verifyToken');
 
 router.get('/products', ProductController.getAll);
 router.post('/products', ProductController.insert);
+router.put('/products', ProductController.update);
+router.delete('/products', ProductController.delete);
 
 module.exports = router;
